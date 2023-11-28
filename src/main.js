8e2ad8fa43c21ce979be40d1a2f988c1600ac843
alasaidi <alasaidi55@gmail.com>
2023-11-28 21:47:29 +0100

    finalize project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 // Get tab elements
 const tabLinks = document.getElementsByClassName("tab-links");
 const tabContents = document.getElementsByClassName("tab-contents");
+const sideMenu = document.getElementById("sideMenu");
+const openIcon = document.querySelector(".fa-bars");
+const closeIcon = document.querySelector(".fa-xmark");
 // Tab toggle function
 function openTab(tabName, e) {
   // Remove active classes
@@ -14,3 +17,36 @@ function openTab(tabName, e) {
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabName).classList.add("active-tab");
 }
+
+openIcon.addEventListener("click", () => {
+  openMenu();
+});
+closeIcon.addEventListener("click", () => {
+  closeMenu();
+});
+const openMenu = () => {
+  sideMenu.style.right = "0";
+};
+
+const closeMenu = () => {
+  sideMenu.style.right = "-200px";
+};
+
+--------
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzJB3GXTebEKbHvIJigwhK_IWR8_pXoiYYGyxi-sPQofRt8yQDwScGAyImsTdYdnGke/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message submitted successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
